28ec761006b2410ead66385c542eb17cd679ae85
Strof15 <70961668+Strof15@users.noreply.github.com>
2023-10-04 10:05:02 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Tabla feladvany;
+        //List<ClassTabla> feladvanybeolvasva = new List<ClassTabla>();
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+    }
+
+        private void feladvanybeolvasasbtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == false)
+            {
+                MessageBox.Show("hibaüzenet");
+            }
+            else
+            {
+                
+
+
+                var fajlsorai = File.ReadAllLines(openFileDialog.FileName);
+
+                byte[,] beMatrix = new byte[fajlsorai.Length,fajlsorai[0].Split().Count()];
+                int x = 0;
+                foreach (var sor in fajlsorai)
+                {
+                    int y = 0;
+                    foreach(var mezo in sor.Split()) 
+                    {
+                        beMatrix[x,y++] = byte.Parse(mezo);
+                    }
+                    x++;
+                }
+                feladvany = new Tabla(beMatrix);
+            }
+            Teruletkirajzolasa(feladvany);
+        }
+
+        private void Teruletkirajzolasa(Tabla feladvany) 
+        {
+        
+        
+        }
+    }
+}
---
+++ Tabla.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Tabla
+    {
+        byte[,] terulet;
+        public Tabla(byte[,] matrix)
+        {
+            terulet = matrix;   
+        }
+
+        public byte[,] Terulet { get => terulet;}
+    }
+}
